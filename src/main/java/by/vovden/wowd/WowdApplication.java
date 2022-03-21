f3379806aeb2378e7b1f7feed864fb79367614ce
Denis <powerdenwin@mail.ru>
2022-03-21 21:31:17 +0300

    start project

---
+++ src/main/java/by/vovden/wowd/WowdApplication.java
@@ -0,0 +1,13 @@
+package by.vovden.wowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/vovden/wowd/WowdApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vovden.wowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
